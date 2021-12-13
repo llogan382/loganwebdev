21c68b82fcf132370482a449d3c29a3659cf2fad
Luke <llogan382@gmail.com>
2021-12-13 09:02:49 -0500

    chroe: updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d8007ff8e186896a"; }
+/******/ 		__webpack_require__.h = function() { return "d1692a9b0e5a0356"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/alert.tsx
+++ components/alert.tsx
@@ -29,14 +29,6 @@ const Alert = ({ preview }: Props) => {
             </>
           ) : (
             <>
-              The source code for this blog is{' '}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-success duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
             </>
           )}
         </div>
