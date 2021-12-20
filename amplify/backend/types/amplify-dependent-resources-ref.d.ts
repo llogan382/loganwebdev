430810066caa1e710a027b14bb5d6287ecbcba4e
Luke <llogan382@gmail.com>
2021-12-20 07:15:50 -0500

    adds analytics

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "loganwebdev": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string"
+        }
+    },
+    "analytics": {
+        "loganwebdevmaster": {
+            "Region": "string",
+            "Id": "string",
+            "appName": "string"
+        }
+    }
+}
