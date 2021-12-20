6ba73f758d0321dd96a2c54e1e674cab71dd0cdf
Luke <llogan382@gmail.com>
2021-12-20 14:57:15 -0500

    updates auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,15 +1,5 @@
 export type AmplifyDependentResourcesAttributes = {
     "auth": {
-        "loganwebdev4738e623": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string",
-            "CreatedSNSRole": "string"
-        },
         "loganwebdevc7ad2e01": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
