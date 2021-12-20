3faf97f7b384bbf574b391903c3ca355e5719ef9
Luke <llogan382@gmail.com>
2021-12-20 11:05:04 -0500

    chore: updates BE

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,15 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "loganwebdev": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string"
-        }
-    },
-    "analytics": {
-        "loganwebdevmaster": {
-            "Region": "string",
-            "Id": "string",
-            "appName": "string"
-        }
-    }
-}
+export type AmplifyDependentResourcesAttributes = {}
