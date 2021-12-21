59c0c8a29be9fa7851aa4a53958662ee19c2f583
Luke <llogan382@gmail.com>
2021-12-21 10:27:31 -0500

    updates analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,72 +8,31 @@ Amplify.configure(awsconfig);
 
 const amplifyConfig = {
   Auth: {
-    identityPoolId: 'COGNITO_IDENTITY_POOL_ID',
+    identityPoolId: 'us-east-1:778adcaa-6e51-4c0b-907b-37d6a2c0e8c7',
     region: 'us-east-1'
   }
 }
 
+// // Initialize the Amazon Cognito credentials provider
+// AWS.config.region = 'us-east-1'; // Region
+// AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+//     IdentityPoolId: 'us-east-1:778adcaa-6e51-4c0b-907b-37d6a2c0e8c7',
+// });
+
 //Initialize Amplify
 Auth.configure(amplifyConfig);
 
-
-
 const analyticsConfig = {
-    autoSessionRecord: true,
-    AWSPinpoint: {
+  AWSPinpoint: {
         // Amazon Pinpoint App Client ID
-        appId: '2e8049eb2c1a47fe8c26579513e27a23',
+        appId: '6b9df0ba8cc64248b5dca18c88a17f6b',
         // Amazon service region
         region: 'us-east-1',
         mandatorySignIn: false,
-            // OPTIONAL -  Amazon service region
-            // OPTIONAL - Default Endpoint Information
-            endpoint: {
-                address: 'xxxxxxx', // The unique identifier for the recipient. For example, an address could be a device token, email address, or mobile phone number.
-                attributes: {
-                    // Custom attributes that your app reports to Amazon Pinpoint. You can use these attributes as selection criteria when you create a segment.
-                    hobbies: ['piano', 'hiking'],
-                },
-                channelType: 'APNS', // The channel type. Valid values: APNS, GCM
-                demographic: {
-                    appVersion: 'xxxxxxx', // The version of the application associated with the endpoint.
-                    locale: 'xxxxxx', // The endpoint locale in the following format: The ISO 639-1 alpha-2 code, followed by an underscore, followed by an ISO 3166-1 alpha-2 value
-                    make: 'xxxxxx', // The manufacturer of the endpoint device, such as Apple or Samsung.
-                    model: 'xxxxxx', // The model name or number of the endpoint device, such as iPhone.
-                    modelVersion: 'xxxxxx', // The model version of the endpoint device.
-                    platform: 'xxxxxx', // The platform of the endpoint device, such as iOS or Android.
-                    platformVersion: 'xxxxxx', // The platform version of the endpoint device.
-                    timezone: 'xxxxxx' // The timezone of the endpoint. Specified as a tz database value, such as Americas/Los_Angeles.
-                },
-                location: {
-                    city: 'xxxxxx', // The city where the endpoint is located.
-                    country: 'xxxxxx', // The two-letter code for the country or region of the endpoint. Specified as an ISO 3166-1 alpha-2 code, such as "US" for the United States.
-                    latitude: 0, // The latitude of the endpoint location, rounded to one decimal place.
-                    longitude: 0, // The longitude of the endpoint location, rounded to one decimal place.
-                    postalCode: 'xxxxxx', // The postal code or zip code of the endpoint.
-                    region: 'xxxxxx' // The region of the endpoint location. For example, in the United States, this corresponds to a state.
-                },
-                metrics: {
-                    // Custom metrics that your app reports to Amazon Pinpoint.
-                },
-                /** Indicates whether a user has opted out of receiving messages with one of the following values:
-                 * ALL - User has opted out of all messages.
-                 * NONE - Users has not opted out and receives all messages.
-                 */
-                optOut: 'ALL',
-                // Customized userId
-                userId: 'XXXXXXXXXXXX',
-                // User attributes
-                userAttributes: {
-                    interests: ['football', 'basketball', 'AWS']
-                    // ...
-                }
-
   }
 }
-};
 
-
+Analytics.configure(analyticsConfig)
 
 Analytics.autoTrack('pageView', {
     // REQUIRED, turn on/off the auto tracking
@@ -103,7 +62,6 @@ Analytics.autoTrack('pageView', {
         return window.location.origin + window.location.pathname;
     }
 });
-Analytics.configure(analyticsConfig);
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
